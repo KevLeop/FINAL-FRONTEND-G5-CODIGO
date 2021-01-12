c6bdffb03dba06b098c2b1c147da74203fff2151
Mateo Quispe Pacheco <mquispe@thomasjefferson.edu.pe>
2021-01-12 18:01:30 -0500

    avance 2 Hclinica

--- app-dental/src/screens/HistoriasClinicas/components/DetalleHClinica.js
+++ app-dental/src/screens/HistoriasClinicas/components/DetalleHClinica.js
@@ -5,7 +5,24 @@ const DetalleHClinica = () => {
     <section className="col-md-3">
       <div className="card shadow">
         <div className="card-body">
-          <strong>Detalle de HistoriaClinica</strong>
+          <form>
+            <div class="form-group">
+            <label htmlfor="">Nombre del Paciente</label>
+              <input type="text" name="" id="hcliPaciente" class="form-control" placeholder="Nombre del Paciente" aria-describedby="helpId"/>
+              <br/>
+              <label htmlfor="">Fecha</label>
+              <input type="date" name="" id="fecha" class="form-control" placeholder="Fecha" aria-describedby="helpId"/>
+              <br/>
+              <label htmlfor="">Problema</label>
+              <input type="text" name="" id="problema" class="form-control" placeholder="Problema" aria-describedby="helpId"/>
+              <br/>
+              <label htmlfor="">Diagnostico</label>
+              <input type="text" name="" id="diagnostico" class="form-control" placeholder="Diagnostico" aria-describedby="helpId"/>
+              <br/> 
+              <label htmlfor="">Tratamiento</label>
+              <input type="text" name="" id="tratamiento" class="form-control" placeholder="Tratamiento" aria-describedby="helpId"/>
+            </div>
+          </form>
         </div>
       </div>
     </section>
