d7cebfaddbba67e9814375db589f1e7d4c7ad705
kevleop <kvalverde@unsa.edu.pe>
2021-04-17 13:30:54 -0500

    registro de paciente completo

--- app-dental/src/modules/auth/screens/register/RegisterScreen.js
+++ app-dental/src/modules/auth/screens/register/RegisterScreen.js
@@ -1,14 +1,73 @@
 import React, { useState } from "react";
 import "../../../../styles/register.css";
-
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { postRegister } from "../../../../services/authService";
+const formularioVacio = {
+  personalNombre: "",
+  personalApellido: "",
+  personalCorreo: "",
+  personalTipo: 1,
+  password: "",
+  personalTelefono: "",
+  passwordConfirm: "",
+};
 const RegisterScreen = () => {
-  const [registerForm, setRegisterForm] = useState({
-    personalCorreo: "",
-    password: "",
-    personalTipo: 1,
-    personalNombre: "",
-    personalApellido: "",
-  });
+  const { register, handleSubmit } = useForm();
+  const [registerForm, setRegisterForm] = useState(formularioVacio);
+
+  const handleChange = (e) => {
+    setRegisterForm({
+      ...registerForm,
+      personalTipo: 1,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (fData) => {
+    console.log(registerForm);
+    Swal.fire({
+      title: "¿Seguro de crear usuario con estos datos",
+      icon: "question",
+      text: "El usuario se registrará en la base de datos",
+      showCancelButton: true,
+    }).then((rpta) => {
+      if (rpta.isConfirmed) {
+        if (registerForm.password === registerForm.passwordConfirm) {
+          postRegister(registerForm).then((data) => {
+            if (data.success) {
+              setRegisterForm(formularioVacio);
+              Swal.fire({
+                title: "Hecho!",
+                text: "Usuario creado con éxito",
+                icon: "success",
+                showCancelButton: false,
+                timer: 1000,
+              });
+            } else {
+              console.log(data.content);
+              Swal.fire({
+                title: "Error!",
+                text: data.content[Object.keys(data.content)[0]],
+                icon: "error",
+                showCancelButton: false,
+                timer: 1600,
+              });
+            }
+          });
+        } else {
+          Swal.fire({
+            title: "Error",
+            text: "Las confirmacion de contraseña no coincide",
+            icon: "error",
+            showCancelButton: false,
+            timer: 1600,
+          });
+        }
+      }
+    });
+  };
 
   return (
     <div className="container register">
@@ -24,71 +83,90 @@ const RegisterScreen = () => {
             Tendrás 30 días de prueba para experimentar un mejor manejo de tus
             citas.
           </p>
-          <input className="btnLogin" type="submit" name="" value="Login" />
+          <Link className="btnLogin" type="submit" name="" to="/">
+            Login
+          </Link>
+          {/* <input className="btnLogin" type="submit" name="" value="Login" /> */}
           <br />
         </div>
         <div className="col-md-9 register-right">
           <div className="tab-content" id="myTabContent">
-            <div
+            {/* <div
               className="tab-pane fade show active"
               id="home"
               role="tabpanel"
               aria-labelledby="home-tab"
-            >
-              <h3 className="register-heading">Ingresa tus datos</h3>
+            > */}
+            <h3 className="register-heading">Ingresa tus datos</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="row register-form">
                 <div className="col-md-6">
                   <div className="form-group">
                     <input
+                      {...register("personalNombre")}
                       type="text"
                       className="form-control"
                       placeholder="Nombres *"
-                      value=""
+                      name="personalNombre"
+                      onChange={handleChange}
+                      value={registerForm.personalNombre}
                     />
                   </div>
                   <div className="form-group">
                     <input
+                      {...register("personalApellido")}
                       type="text"
                       className="form-control"
                       placeholder="Apellidos *"
-                      value=""
+                      name="personalApellido"
+                      onChange={handleChange}
+                      value={registerForm.personalApellido}
                     />
                   </div>
                   <div className="form-group">
                     <input
+                      {...register("password")}
                       type="password"
                       className="form-control"
                       placeholder="Contraseña *"
-                      value=""
+                      name="password"
+                      onChange={handleChange}
+                      value={registerForm.password}
                     />
                   </div>
                 </div>
                 <div className="col-md-6">
                   <div className="form-group">
                     <input
+                      {...register("personalCorreo")}
                       type="email"
                       className="form-control"
                       placeholder="Correo Electrónico"
-                      value=""
+                      name="personalCorreo"
+                      onChange={handleChange}
+                      value={registerForm.personalCorreo}
                     />
                   </div>
                   <div className="form-group">
                     <input
                       type="text"
-                      minlength="10"
-                      maxlength="10"
-                      name="txtEmpPhone"
+                      minlength="9"
+                      maxlength="12"
                       className="form-control"
                       placeholder="Telefono"
-                      value=""
+                      onChange={handleChange}
+                      value={registerForm.personalTelefono}
                     />
                   </div>
                   <div className="form-group">
                     <input
+                      {...register("passwordConfirm")}
                       type="password"
                       className="form-control"
                       placeholder="Confirma tu contraseña *"
-                      value=""
+                      name="passwordConfirm"
+                      onChange={handleChange}
+                      value={registerForm.passwordConfirm}
                     />
                   </div>
                   <input
@@ -98,7 +176,8 @@ const RegisterScreen = () => {
                   />
                 </div>
               </div>
-            </div>
+            </form>
+            {/* </div> */}
           </div>
         </div>
       </div>
--- app-dental/src/services/authService.js
+++ app-dental/src/services/authService.js
@@ -11,3 +11,15 @@ export const postLogin = async (objAuth) => {
   let data = await peticion.json();
   return data;
 };
+
+export const postRegister = async (objUsuario) => {
+  const peticion = await fetch(`${URL_BACKEND}/registro`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(objUsuario),
+  });
+  let data = await peticion.json();
+  return data;
+};
